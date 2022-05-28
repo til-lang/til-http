651d55e690df0ea7c0366edb811bcb9ca322e1df
Cléber Zavadniak <contato@cleber.solutions>
2022-05-27 22:45:10 -0300

    code improvements

--- source/app.d
+++ source/app.d
@@ -1,7 +1,52 @@
 import std.net.curl;
-import std.stdio;
 
 import til.nodes;
+
+
+struct ret
+{
+    HTTP http;
+    Context context;
+}
+
+
+auto getHttp(Context context)
+{
+    auto http = HTTP();
+
+    // Headers:
+    while (context.size > context.inputSize)
+    {
+        // (header value)
+        auto pair = context.pop!SimpleList();
+        auto returnedContext = pair.evaluate(context, true);
+        pair = returnedContext.pop!SimpleList();
+
+        if (pair.items.length != 2)
+        {
+            return ret(http, context.error(
+                "Headers arguments should be pairs of key / value",
+                ErrorCode.InvalidSyntax,
+                "http"
+            ));
+        }
+        auto key = pair.items[0].toString();
+        auto value = pair.items[1].toString();
+        http.addRequestHeader(key, value);
+    }
+
+    // Body:
+    if (context.inputSize)
+    {
+        http.setPostData(context.pop!string(), "application/json");
+    }
+    else
+    {
+        http.setPostData(null, "application/json");
+    }
+
+    return ret(http, context);
+}
 
 
 extern (C) CommandsMap getCommands(Escopo escopo)
@@ -11,71 +56,31 @@ extern (C) CommandsMap getCommands(Escopo escopo)
     commands["get"] = new Command((string path, Context context)
     {
         string address = context.pop!string();
-        auto http = HTTP();
-
-        foreach (item; context.items)
+        ret r = getHttp(context);
+        if (r.context.exitCode == ExitCode.Failure)
         {
-            // (header value)
-            auto pair = cast(SimpleList)item;
-            auto returnedContext = pair.evaluate(context, true);
-            pair = returnedContext.pop!SimpleList();
-
-            if (pair.items.length != 2)
-            {
-                return context.error(
-                    "Headers arguments should be pairs of key / value",
-                    ErrorCode.InvalidSyntax,
-                    "http"
-                );
-            }
-            auto key = pair.items[0].toString();
-            auto value = pair.items[1].toString();
-            http.addRequestHeader(key, value);
+            return context;
         }
 
-        auto content = get(address, http);
-        return context.push(to!string(content));
+        auto content = get(address, r.http);
+        return r.context.push(to!string(content));
     });
     commands["post"] = new Command((string path, Context context)
     {
         // http.post $url "header:value" [dict (json values) (goes here)]
         // (And form-encoded will work, eventually, but now today.)
         string address = context.pop!string();
-        auto http = HTTP();
 
-        while (context.size > context.inputSize)
+        ret r = getHttp(context);
+        if (r.context.exitCode == ExitCode.Failure)
         {
-            // (header value)
-            auto pair = context.pop!SimpleList();
-            auto returnedContext = pair.evaluate(context, true);
-            pair = returnedContext.pop!SimpleList();
-
-            if (pair.items.length != 2)
-            {
-                return context.error(
-                    "Headers arguments should be pairs of key / value",
-                    ErrorCode.InvalidSyntax,
-                    "http"
-                );
-            }
-            auto key = pair.items[0].toString();
-            auto value = pair.items[1].toString();
-            http.addRequestHeader(key, value);
+            return context;
         }
 
-        if (context.inputSize)
-        {
-            http.setPostData(context.pop!string(), "application/json");
-        }
-        else
-        {
-            http.setPostData(null, "application/json");
-        }
+        auto content = post(address, [], r.http);
+        auto s = to!string(content);
 
-        writeln("Sending the POST request: ", address);
-        auto content = post(address, [], http);
-        writeln("Returned.");
-        return context.push(to!string(content));
+        return r.context.push(s);
     });
 
     return commands;
